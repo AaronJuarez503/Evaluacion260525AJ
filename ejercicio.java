54dd13011c319f6b759fea674e0eda07a2025e33
Jazmin <ACER@LAPTOP-FRMPB979>
2025-05-25 15:49:43 -0600

    clases de futbolista y masajista

--- ejercicio.java
+++
@@ -1,9 +0,0 @@
-public class ejercicio {
-    public static void main(String[] args) {
-        // Imprimir números del 1 al 10
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
-    }
-    
-}
