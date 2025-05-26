7cf94f86ecce3d3bd3138f618433cab823efa42b
Jazmin <eugenialue503@gmail.com>
2025-05-26 11:02:49 -0600

    a

--- Masajista.java
+++ Masajista.java
@@ -1,9 +1,11 @@
 public class Masajista extends Persona {
     private String especialidad;
+    private int aniosExperiencia;
 
-    public Masajista(String nombre, int edad, String especialidad) {
-        super(nombre, edad);
+    public Masajista(int id, String nombre, String apellidos, int edad, String especialidad, int aniosExperiencia) {
+        super(id, nombre, apellidos, edad);
         this.especialidad = especialidad;
+        this.aniosExperiencia = aniosExperiencia;
     }
 
     public String getEspecialidad() {
@@ -12,6 +14,28 @@ public class Masajista extends Persona {
 
     public void setEspecialidad(String especialidad) {
         this.especialidad = especialidad;
+    }
+
+    public int getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+
+    public void setAniosExperiencia(int aniosExperiencia) {
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    @Override
+    public void concentrarse() {
+        System.out.println(getNombre() + " (Masajista) se está concentrando.");
+    }
+
+    @Override
+    public void viajar() {
+        System.out.println(getNombre() + " (Masajista) está viajando.");
+    }
+
+    public void darMasaje() {
+        System.out.println(getNombre() + " está dando un masaje.");
     }
 
     @Override
